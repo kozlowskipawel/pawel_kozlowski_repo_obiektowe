5f3cc9bc13a111c49d73d14a3610a8a4f9666e94
kozlowskipawel <kozlowskipawel_1@wp.pl>
2022-01-05 09:30:22 +0100

    pisanie zadan

--- 1.6 zadanie/zadanie1.java
+++ 1.6 zadanie/zadanie1.java
@@ -1,7 +1,24 @@
-public class Zadanie1
-{
-    public static void main(String[] args)
-    {
-        
+import java.util.Scanner;
+
+class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj haslo");
+        String haslo = sc.nextLine();
+        int dlugosc = haslo.length();
+        String pierwszeHaslo = haslo.substring(0, dlugosc);
+        System.out.println("Powtórz haslo");
+        haslo = sc.nextLine();
+        dlugosc = haslo.length();
+        dlugosc = haslo.length();
+        String drugieHaslo = haslo.substring(0, dlugosc);
+
+        if (pierwszeHaslo.equals(drugieHaslo)) {
+            System.out.println("Hasla sa poprawne");
+
+        } else {
+            System.out.println("Hasla sa rozne");
+        }
+
     }
 }
